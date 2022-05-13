99b0eb000c4d9626446d80d0edc2f8e3b47ef85b
9595kys <9595kys@gmail.com>
2022-05-13 21:26:09 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
+CORS(app)
+
+db = SQLAlchemy(app)
+
+class Shop():
+    id = db.Column(db.Integer, primery_key=True, autoincrement=False)
+    shop_name = db.Column(db.String(200))
+    shop_address = db.Column(db.String(200))
+
+class Order():
+    id = db.Column(db.Integer, primery_key=True, autoincrement=False)
+    shop = db.Column(db.Integer)
+    delivery_address = db.Column(db.String(200))
+
+
+@app.route('/')
+def index():
+    return 'hello'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
