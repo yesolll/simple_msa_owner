c865881f134c82877264966906c97c0cc8d41dd4
9595kys <9595kys@gmail.com>
2022-05-13 22:00:07 +0900

    db migration

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 CORS(app)
 
 db = SQLAlchemy(app)
 
 class Shop():
-    id = db.Column(db.Integer, primery_key=True, autoincrement=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     shop_name = db.Column(db.String(200))
     shop_address = db.Column(db.String(200))
 
 class Order():
-    id = db.Column(db.Integer, primery_key=True, autoincrement=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     shop = db.Column(db.Integer)
     delivery_address = db.Column(db.String(200))
 
